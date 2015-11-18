1aeb35ecc95082f2f23bf6c45228935b6f966e42
jinah adam <jinahadam@me.com>
2015-11-18 16:49:45 +0800

    initial commit

---
+++ scripts/joyfailsafe.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import Int8
+import pygame as pg
+
+def talker():
+    pub = rospy.Publisher('failsafe', Int8, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1) # 10hz
+    while not rospy.is_shutdown():
+         pg.init()
+         pg.joystick.init()
+    	 joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
+  	 if len(joysticks) > 0:
+             pub.publish(1)
+         else:
+             pub.publish(0)
+         pg.quit()
+         rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
